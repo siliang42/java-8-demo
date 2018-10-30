14ced07548b2c5471396b29f02e10a8d46a8d445
siliang42 <obanie@qq.com>
2018-10-31 01:27:52 +0800

    add more

--- src/main/java/com/jiayi/Java8Demo.java
+++ src/main/java/com/jiayi/Java8Demo.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
 
 /**
@@ -42,11 +43,17 @@ public class Java8Demo {
      *   TreeSet<Integer> ts = new TreeSet<Integer>();
      *   }
      */
-
+    List<Employee> employees = Arrays.asList(
+            new Employee("zhangsan", 18, 999.99),
+            new Employee("lisi", 20, 666.66),
+            new Employee("wantgwu", 50, 555.55),
+            new Employee("zhaoliu", 40, 777.77),
+            new Employee("tianqi", 35, 333.33)
+    );
 
 
     //Demand : get employees whose age more than 35
-    public List<Employee> filterEmployees(List<Employee> list){
+    public List<Employee> filterEmployeesAgeMoreThan35(List<Employee> list){
         List<Employee> emps1 = new ArrayList<>();
 
         for (Employee employee : list) {
@@ -59,16 +66,54 @@ public class Java8Demo {
         return  emps1;
     }
 
+    //Demand : get employees whose age more than 35
+    public List<Employee> filterEmployeesSalaryMoreThan555(List<Employee> list){
+        List<Employee> emps1 = new ArrayList<>();
+
+        for (Employee employee : list) {
+            if(employee.getSalary()> 555){
+                emps1.add(employee);
+            }
+
+        }
+
+        return  emps1;
+    }
+
+    public List<Employee> filterEmployee(List<Employee> list, MyPredicate<Employee> myPredicate){
+        List<Employee> emps = new ArrayList<>();
+
+        for (Employee employee : list) {
+            if(myPredicate.test(employee)){
+                emps.add(employee);
+            }
+        }
+        return  emps;
+    }
+
     @Test
     public void test03(){
-        List<Employee> employees = Arrays.asList(
-                new Employee("zhangsan", 18, 999.99),
-                new Employee("lisi", 20, 999.99),
-                new Employee("wantgwu", 50, 999.99),
-                new Employee("zhaoliu", 40, 999.99),
-                new Employee("tianqi", 35, 999.99)
-        );
-        List<Employee> emps = filterEmployees(employees);
+
+        List<Employee> emps = filterEmployeesAgeMoreThan35(employees);
+        String s = GsonUtil.toJson(emps);
+        System.out.printf(s);
+    }
+    @Test
+    public void test04(){
+        List<Employee> emps = filterEmployeesSalaryMoreThan555(employees);
+        String s = GsonUtil.toJson(emps);
+        System.out.printf(s);
+    }
+
+    @Test
+    public void test05(){
+        List<Employee> emps = filterEmployee(employees, new MyPredicateByAge());
+        String s = GsonUtil.toJson(emps);
+        System.out.printf(s);
+    }
+    @Test
+    public void test06(){
+        List<Employee> emps = filterEmployee(employees, new MyPredicateBySalary());
         String s = GsonUtil.toJson(emps);
         System.out.printf(s);
     }
